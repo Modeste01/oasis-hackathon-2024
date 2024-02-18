ffbbfb7e62c6574998d1fb9ae24a1d53d80807ad
Modeste01 <108250043+Modeste01@users.noreply.github.com>
2024-02-18 00:08:10 -0800

    Added substitutes to already present sentences

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -74,22 +74,52 @@ public class GameManager : MonoBehaviour
             {
                 case 0:
                     // Robber takes food
-                    eventText.text = "You are held at gunpoint and a group of bandits steal your food.";
+
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "You are held at gunpoint and a group of bandits steal your food.";
+                    }
+                    else
+                    {
+                        eventText.text = "You are under the threat of a firearm, and a band of outlaws snatches your food provisions from you.";
+                    }
                     foodReserve -= Random.Range(1, 4);
                     break;
                 case 1:
                     // Robber takes water
-                    eventText.text = "You are held at gunpoint and a group of bandits steal your water.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "You are held at gunpoint and a group of bandits steal your water.";
+                    }
+                    else
+                    {
+                        eventText.text = "You are under the threat of a firearm, and a band of outlaws steals from you one a significant water reserve.";
+                    }
                     waterReserve -= Random.Range(2, 5);
                     break;
                 case 2:
                     // Come across oasis
-                    eventText.text = "Your friend catches and prepares a quail and everyone gets a nice meal.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "Your friend catches and prepares a quail and everyone gets a nice meal.";
+                    }
+                    else
+                    {
+                        eventText.text = "Your companion snares and cooks a sandgrouse, allowing for everyone to have a delightful supper.";
+                    }
                     foodReserve += Random.Range(1, 3);
                     break;
                 case 3:
                     // Robber takes water
-                    eventText.text = "Lucky you, you come across an oasis and found some water.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "Lucky you, you come across an oasis and found some water.";
+                    }
+                    else
+                    {
+                        eventText.text = "You stumble upon a spring and accidently discover some hydration.";
+                    }
+                    
                     waterReserve += Random.Range(2, 4);
                     break;
                 default:
@@ -106,19 +136,55 @@ public class GameManager : MonoBehaviour
             switch(Random.Range(0, 5))
             {
                 case 0:
-                    eventText.text = "You stop to take a short break.";
+                    if(Random.Range(0,2) == 0)
+                    {
+                        eventText.text = "You stop to take a short break.";
+                    }
+                    else
+                    {
+                        eventText.text = "You pause for a brief respite.";
+                    }
                     break;
                 case 1:
-                    eventText.text = "You pause to watch, a crow flies overheard.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "You pause to watch, a crow flies overheard.";
+                    }
+                    else
+                    {
+                        eventText.text = "You momentarily halt, as an eagle glides overhead.";
+                    }
                     break;
                 case 2:
-                    eventText.text = "You pause to watch, a falcon flies overheard.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "You pause to watch, a falcon flies overheard.";
+                    }
+                    else
+                    {
+                        eventText.text = "You halt your journey, as a hawk soars in the sky above.";
+                    }
+                        
                     break;
                 case 3:
-                    eventText.text = "Your friend notices a poisonous snake in the distance.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "Your friend notices a poisonous snake in the distance.";
+                    }
+                    else
+                    {
+                        eventText.text = "In a distance, your friend notices a poisonous snake.";
+                    }  
                     break;
                 case 4:
-                    eventText.text = "You start noticing figures in the sand, it's time to take a break.";
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        eventText.text = "You start noticing figures in the sand, it's time to take a break.";
+                    }
+                    else
+                    {
+                        eventText.text = "You begin to see shapes in the sand, it's time for a respite.";
+                    }      
                     break;
                 default:
                     eventText.text = "You notice a sandworm making its way to you.";
