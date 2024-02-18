6c189a1f0b8d8969f48e496a87698e792d6d12c1
HappyGamer3194 <aryanputhran@gmail.com>
2024-02-17 16:08:26 -0800

    Third Commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,38 +20,95 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int waterReserve;
     [Range(0, 1)] [SerializeField] private float threshold;
     [SerializeField] private float distanceLeft;
+    [SerializeField] private float betweenEventDelay;
 
-    private float randomNumber;
-    private int eventId;
+    private bool isGameStarted;
+    private bool isInEvent;
 
     // Start is called before the first frame update
     void Start()
     {
-        threshold = 0;
+        isGameStarted = false;
+        isInEvent = false;
+
+        introCanvas.SetActive(true);
+        eventCanvas.SetActive(false);
+        dieCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Start the game, if not started yet
+        if (Input.GetKeyDown(KeyCode.Space) && !isGameStarted)
+        {
+            isGameStarted = true;
+            introCanvas.SetActive(false);
+
+            eventCanvas.SetActive(true);
+            eventText.text = "Click SPACE to start the journey.";
+        }
+
         // Update slides to match variables
         foodSlider.value = foodReserve;
         waterSlider.value = waterReserve;
+
+        // Reset if the player is currently in an event if space is pressed
+        if (Input.GetKeyDown(KeyCode.Space) && !isInEvent && isGameStarted)
+        {
+            StartCoroutine(RandomEvent(Random.Range(0, 2)));
+        }
     }
 
-    void RandomEvent()
+    IEnumerator RandomEvent(int eventID)
     {
-        switch(eventId)
+        isInEvent = true;
+
+        if (Random.Range(0.0f, 1.0f) <= threshold)
         {
-            case 0:
-                // Robber takes food
-
-                break;
-            case 1:
-                // Robber takes water
-
-                break;
-            default:
-                break;
+            switch (eventID)
+            {
+                case 0:
+                    // Robber takes food
+                    eventText.text = "You are held at gunpoint and a group of bandits steal your food.";
+                    foodReserve -= Random.Range(1, 4);
+                    break;
+                case 1:
+                    // Robber takes water
+                    eventText.text = "You are held at gunpoint and a group of bandits steal your water.";
+                    waterReserve -= Random.Range(2, 5);
+                    break;
+                default:
+                    break;
+            }
         }
+        else
+        {
+            // Nothing happens
+            switch(Random.Range(0, 5))
+            {
+                case 0:
+                    eventText.text = "You stop to take a short break.";
+                    break;
+                case 1:
+                    eventText.text = "You pause to watch, a crow flies overheard.";
+                    break;
+                case 2:
+                    eventText.text = "You pause to watch, a falcon flies overheard.";
+                    break;
+                case 3:
+                    eventText.text = "Your friend notices a poisonous snake in the distance.";
+                    break;
+                case 4:
+                    eventText.text = "You start noticin figures in the sand, it's time to take a break.";
+                    break;
+                default:
+                    eventText.text = "You notice a sandworm making its way to you.";
+                    break;
+            }
+        }
+
+        yield return new WaitForSeconds(betweenEventDelay);
+        isInEvent = false;
     }
 }
