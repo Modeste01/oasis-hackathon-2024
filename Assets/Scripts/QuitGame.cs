f03c6f436eb34c85fc346c6097a882e1fbc2f7f7
HappyGamer3194 <aryanputhran@gmail.com>
2024-02-17 16:33:50 -0800

    Added Credits

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // Function that quits the game
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    } 
+}
